848081c681862272764fcf8090125142273d386c
atticusmatticus <20863400+atticusmatticus@users.noreply.github.com>
2026-03-04 16:52:40 -0600

    Updated colors and some hilight linking mostly for markdown

--- lua/dracula/palette.lua
+++ lua/dracula/palette.lua
@@ -23,29 +23,48 @@
 ---@field nontext string
 ---@field white string
 ---@field black string
+---@field dim_red string
+---@field dim_green string
+---@field dim_yellow string
+---@field dim_blue string
+---@field dim_magenta string
+---@field dim_cyan string
+---@field dim_white string
 return {
-   bg = "#282A36",
-   fg = "#F8F8F2",
-   selection = "#44475A",
-   comment = "#6272A4",
-   red = "#FF5555",
-   orange = "#FFB86C",
-   yellow = "#F1FA8C",
-   green = "#50fa7b",
-   purple = "#BD93F9",
-   cyan = "#8BE9FD",
-   pink = "#FF79C6",
-   bright_red = "#FF6E6E",
-   bright_green = "#69FF94",
-   bright_yellow = "#FFFFA5",
-   bright_blue = "#D6ACFF",
-   bright_magenta = "#FF92DF",
-   bright_cyan = "#A4FFFF",
-   bright_white = "#FFFFFF",
+   bg = "#22212c",
+   fg = "#f8f8f2",
+   selection = "#454158",
+   comment = "#7970a9",
+
+   red = "#ff9580",
+   orange = "#ffca80",
+   yellow = "#ffff80",
+   green = "#8aff80",
+   purple = "#9580ff",
+   cyan = "#80ffea",
+   pink = "#ff80bf",
+
+   bright_red = "#ffaa99",
+   bright_green = "#a2ff99",
+   bright_yellow = "#ffff99",
+   bright_blue = "#aa99ff",
+   bright_magenta = "#ff99cc",
+   bright_cyan = "#99ffee",
+   bright_white = "#ffffff",
    menu = "#21222C",
-   visual = "#3E4452",
+   visual = "#662222",
    gutter_fg = "#4B5263",
-   nontext = "#3B4048",
-   white = "#ABB2BF",
+   nontext = "#4f4c65",
+
+   white = "#f8f8f2",
    black = "#191A21",
+
+   dim_black = "#1B1A23",
+   dim_red = "#CC7766",
+   dim_green = "#6ECC66",
+   dim_yellow = "#CCCC66",
+   dim_blue = "#7766CC",
+   dim_magenta = "#CC6699",
+   dim_cyan = "#66CCBB",
+   dim_white = "#C6C6C2",
 }
